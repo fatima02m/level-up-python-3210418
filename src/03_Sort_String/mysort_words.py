fc5d0eb50b6603444ffe191ad2f26af17cdab1bc
fatima02m <95159942+fatima02m@users.noreply.github.com>
2023-09-18 23:25:58 +0000

    finished sorting words alphabetically

---
+++ src/03_Sort_String/mysort_words.py
@@ -0,0 +1,15 @@
+# sorts words in a string alphabetically
+
+import string
+
+
+def sort_string(str):
+    # splits the string into words
+    list = str.split()
+    # sorts them alphabetically using built in function sorted
+    sorted_str = sorted(list)
+    return sorted_str
+
+
+if __name__ == '__main__':
+    print(sort_string('j a l e m e y'))
